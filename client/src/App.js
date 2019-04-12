7ce8c6995d0f5f8b71234a745695e748e872197e
Anton <a-pach@yandex.ru>
2019-04-12 15:31:45 +0300

    nginx default.conf changed

--- client/src/App.js
+++ client/src/App.js
@@ -12,37 +12,18 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Welcome to Fibonacci!</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
-          <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+          <div>            
+              <Route exact path="/" component={Fib} />
+              <Route path="/otherpage" component={OtherPage} />            
           </div>
         </div>
       </Router>
     );
   }
 }
-
-exports.error404 = function(req, res) {
-  if (req.accepts('html')) {
-      // Respond with html page.
-      fs.readFile(__dirname + '/../../public/404/index.html', 'utf-8', function(err, page) {
-          res.writeHead(404, {'Content-Type': 'text/html'});
-          res.write(page);
-          res.end();
-      });
-  }
-  else if (req.accepts('json')) {
-      // Respond with json.
-      res.status(404).send({ error: 'Not found' });
-  }
-  else {
-      // Default to plain-text. send()
-      res.status(404).type('txt').send('Not found');
-  }
-};
 
 export default App;
